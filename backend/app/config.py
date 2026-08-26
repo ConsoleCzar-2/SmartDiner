8b98d9bd28cd6fba9324be13bdff66567f659588
Abhirup Saha <179203311+ConsoleCzar-2@users.noreply.github.com>
2026-08-26 23:48:48 +0530

    Minor fixes

--- backend/app/config.py
+++ backend/app/config.py
@@ -47,7 +47,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(".env", "../.env"),
         env_file_encoding="utf-8",
-        case_sensitive=False
+        case_sensitive=False,
+        extra="ignore",
     )
 
     @field_validator("cors_origins", mode="before")
